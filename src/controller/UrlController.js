ff7eb06fc390e4d5f9f71e6e46f86e000c6422e1
Duttji1996 <prakashdutttripathi@gmail.com>
2022-05-20 12:31:51 +0530

    okay

--- src/controller/UrlController.js
+++ src/controller/UrlController.js
@@ -24,39 +24,31 @@ const SETEX_ASYNC = promisify(redisClient.SETEX).bind(redisClient);
 const CreateShortUrl = async function (req, res) {
   try {
     let body = req.body;
-    // let longUrl = body.longUrl
+    let longUrl = body.longUrl
 
     if (Object.keys(body).length === 0) {
-      return res
-        .status(400)
-        .send({ status: false, message: "Please enter the data" });
-    }
-    if (!body.longUrl) {
-      return res
-        .status(400)
-        .send({ status: false, message: "Please enter the URL" });
-    }
-    if (!validUrl.isWebUri(body.longUrl)) {
-      console.log("Looks like not a valid URL");
-      return res
-        .status(400)
-        .send({ status: false, message: "Looks like not a valid URL" });
+      return res.status(400).send({ status: false, message: "Please enter the data" });
     }
 
-    let getUrl = await GET_ASYNC(`${body.longUrl}`);
-    let url = JSON.parse(getUrl);
-    if (url) {
-      return res
-        .status(200)
-        .send({ status: true, message: "success", data: url });
+    if (!body.longUrl) {
+      return res.status(400).send({ status: false, message: "Please enter the URL" });
     }
+
+    if (!validUrl.isWebUri(body.longUrl)) {
+     // console.log("Looks like not a valid URL");
+      return res.status(400).send({ status: false, message: "Looks like not a valid URL" });
+    }
+
+    // let getUrl = await GET_ASYNC(`${body.longUrl}`);
+    // let url = JSON.parse(getUrl);
+    // if (url) {
+    //   return res.status(200).send({ status: true, message: "success", data: url });
+    // }
 
     let FindUrl = await urlModel.findOne({ longUrl: body.longUrl }); //.select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 })
 
     if (FindUrl) {
-      return res
-        .status(400)
-        .send({ status: false, message: "this urlcode is already generated" });
+    return res.status(400).send({ status: false, message: "this urlcode is already generated" });
     }
     const urlCode = shortid.generate().toLowerCase();
 
@@ -64,22 +56,18 @@ const CreateShortUrl = async function (req, res) {
 
     let shortUrl = baseUrl + "/" + urlCode;
 
-    // url = { longUrl, shortUrl, urlCode }
-    body.shortUrl = shortUrl;
-    body.urlCode = urlCode;
+    url = { longUrl, shortUrl, urlCode }
+    // body.shortUrl = shortUrl;
+    // body.urlCode = urlCode;
 
-    await urlModel.create(body);
+    await urlModel.create(url);
 
-    let ShowUrl = await urlModel
-      .findOne({ longUrl: body.longUrl })
-      .select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 });
+    let ShowUrl = await urlModel.findOne({ longUrl: body.longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 });
 
     await SETEX_ASYNC(`${body.longUrl}`, 3600, JSON.stringify(ShowUrl));
-    res.status(201).send({
-      status: true,
-      message: "URL create successfully",
-      body: shortUrl,
-    });
+
+    res.status(201).send({status: true,message: "URL create successfully",data: ShowUrl});
+
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
@@ -91,18 +79,17 @@ const GetUrl = async function (req, res) {
     let url = JSON.parse(getLongUrl);
     if (url) {
       return res.status(307).redirect(url.longUrl);
-    } else {
+    } 
+    else {
       let getUrl = await urlModel.findOne({ urlCode: req.params.urlCode });
       if (!getUrl) {
-        return res
-          .status(404)
-          .send({ status: false, message: "Url-Code not found" });
+        return res.status(404).send({ status: false, message: "Url-Code not found" });
       }
 
       await SETEX_ASYNC(`${req.params.urlCode}`, 3600, JSON.stringify(getUrl));
       return res.status(307).redirect(getUrl.longUrl);
     }
-  } catch (error) {
+  }catch (error) {
     res.status(500).send({ status: false, message: error.message });
   }
 };
